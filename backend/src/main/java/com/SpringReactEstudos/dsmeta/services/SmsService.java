ba25cf648e143664d2115221cd3080cd7172ff3d
S-crip-eer-tech <ReynaldoScrip@Outlook.com>
2022-09-10 03:38:14 -0300

    Hospedagem/Sms

--- backend/src/main/java/com/SpringReactEstudos/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/SpringReactEstudos/dsmeta/services/SmsService.java
@@ -1,5 +1,6 @@
 package com.SpringReactEstudos.dsmeta.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -36,8 +37,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " +  date
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 	
