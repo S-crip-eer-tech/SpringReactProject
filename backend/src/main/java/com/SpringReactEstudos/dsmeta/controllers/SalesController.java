d4b55a3cb4917fae42b5a16e5964102cc3d49f26
S-crip-eer-tech <ReynaldoScrip@Outlook.com>
2022-09-10 00:18:44 -0300

    Date select

--- backend/src/main/java/com/SpringReactEstudos/dsmeta/controllers/SalesController.java
+++ backend/src/main/java/com/SpringReactEstudos/dsmeta/controllers/SalesController.java
@@ -1,12 +1,12 @@
 package com.SpringReactEstudos.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.SpringReactEstudos.dsmeta.entities.Sale;
 import com.SpringReactEstudos.dsmeta.services.SaleService;
@@ -19,8 +19,11 @@ public class SalesController {
 	private SaleService service;
 	
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 
 }
--- backend/src/main/java/com/SpringReactEstudos/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/SpringReactEstudos/dsmeta/services/SaleService.java
@@ -1,19 +1,32 @@
 package com.SpringReactEstudos.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.SpringReactEstudos.dsmeta.entities.Sale;
 import com.SpringReactEstudos.dsmeta.repositories.SaleRepository;
+
 
 @Service
 public class SaleService {
 	
 	@Autowired
 	private SaleRepository repository;
-	public List<Sale> findSales() {
-		return repository.findAll();
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pegeable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pegeable);
 	}
 }
